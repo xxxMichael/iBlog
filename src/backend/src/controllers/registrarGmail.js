70f5b44909d0e59dade692e59b814cafd9d57c4d
xxxMichael <mayckolchavez27@gmail.com>
2024-05-17 11:55:01 -0500

    regGmailv1

--- src/backend/src/controllers/registrarGmail.js
+++ src/backend/src/controllers/registrarGmail.js
@@ -1,6 +1,6 @@
 const connection = require('../models/db');
 
-module.exports.verfRegistro = (data) => {
+module.exports.registrarGmail = (data) => {
     const query = `
     INSERT INTO usuarioAutenticado (nombre, apellido, username, email, contra, fechaRegistro, rol, fechaNac, pais)
     VALUES (?, ?, ?, ?, ?, CURRENT_DATE, 'Bronce', ?, ?)
@@ -14,6 +14,11 @@ module.exports.verfRegistro = (data) => {
     data.fechaNac || null, // Ajusta según tus necesidades si tienes la fecha de nacimiento
     data.pais || null // Ajusta según tus necesidades si tienes el país
   ];
+  console.log(data.nombre,
+    data.apellido,
+    data.username,
+    data.email,
+    data.password);
   connection.query(query, values, (err, result) => {
     if (err) {
       console.error('Error inserting user:', err);
