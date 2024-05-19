87ef9589def895aabfea2c0e2a06ce8de8f41775
Axios21 <davidgiler21@gmail.com>
2024-05-19 08:53:45 -0500

    incorporarAtropos Register

--- src/componentesFront/Login/Registro.jsx
+++ src/componentesFront/Login/Registro.jsx
@@ -167,38 +167,42 @@ const Registro = ({ handleBackToLoginClick, handleSignUp }) => {
 
   return (
     <div className="Registro_C">
-      <h1 className="titulo">REGISTRO</h1>
+      <h1 data-atropos-offset="7" className="titulo">REGISTRO</h1>
+      <button data-atropos-offset="20" className="btnRegresoLogin" onClick={handleBackToLoginClick}>
+        <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+        <div className="text">Return to Login</div>
+      </button>
       <form className="registration-form" onSubmit={handleSignUpSubmit}>
         <div className="input-column">
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <span>Gmail</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <span>Password</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
             <span>Name</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
             <span>Confirm Password</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
             <span>Lastname</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input required type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
             <span>Username</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <input className="fecha" required type="date" id="dob" value={dob} onChange={(e) => setDob(e.target.value)} />
             <span>Birthdate</span>
           </div>
-          <div className="inputBox">
+          <div data-atropos-offset="7" className="inputBox">
             <select
               id="country"
               value={country}
@@ -228,14 +232,14 @@ const Registro = ({ handleBackToLoginClick, handleSignUp }) => {
             </select>
           </div>
         </div>
-        <div className="button-container">
+        <div data-atropos-offset="5" className="button-container">
           <button className="btnRegisterE">
             <span>Register</span><i></i>
           </button>
         </div>
       </form>
       {isVisible && (
-        <div className="mensageError"
+        <div data-atropos-offset="7" className="mensageError"
           style={{
             backgroundColor: getColor(),
             padding: "10px",
@@ -248,9 +252,6 @@ const Registro = ({ handleBackToLoginClick, handleSignUp }) => {
           {errorMessage.message}
         </div>
       )}
-      <button className="btnRegresoLogin" onClick={handleBackToLoginClick}>
-        Regresar a inicio de sesión
-      </button>
     </div>
   );
 };
