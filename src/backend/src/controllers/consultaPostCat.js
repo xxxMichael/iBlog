a758939645cc32a8eb0508a4faef2df110a9fbc2
xxxMichael <mayckolchavez27@gmail.com>
2024-05-26 13:51:22 -0500

    actPostsbyCategoria

--- src/backend/src/controllers/consultaPostCat.js
+++ src/backend/src/controllers/consultaPostCat.js
@@ -11,10 +11,9 @@ module.exports.consultaPostCat = (req, res) => {
     SELECT p.idPost, p.dueño, p.titulo, p.contenido, p.fechaPublicacion, p.urlImagen, p.urlDocumento
     FROM posts p
     WHERE p.idCategoria1 = ? OR p.idCategoria2 = ? OR p.idCategoria3 = ?;
-    
     `;
 
-    connection.query(query, [categoriaId], (error, results) => {
+    connection.query(query, [categoriaId, categoriaId, categoriaId], (error, results) => {
         if (error) {
             console.error('Error al obtener los posts:', error);
             return res.status(500).json({ error: 'Error al obtener los posts' });
