050c85faf04a555259d997115e181e733c1338e6
xxxMichael <mayckolchavez27@gmail.com>
2024-05-26 14:19:28 -0500

    cerrar sesion

--- src/componentesFront/Home/Home.jsx
+++ src/componentesFront/Home/Home.jsx
@@ -1,140 +1,185 @@
 /* eslint-disable no-unused-vars */
 // src/componentesFront/Home/Home.jsx
-import './Home.css';
-import { useEffect, useState } from 'react'; // Importa useEffect y useState
-import { parseJwt } from '../Main/Main'; // Asegúrate de importar la función parseJwt desde el archivo correcto
-import { Link, Router } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa';
-import { FaHome, FaUser } from 'react-icons/fa';
-import Categorias from './categorias.jsx';
-import axios from 'axios';
-import Formulario from '../Home/formularioPost.jsx';
+import "./Home.css";
+import { useEffect, useState } from "react"; // Importa useEffect y useState
+import { parseJwt } from "../Main/Main"; // Asegúrate de importar la función parseJwt desde el archivo correcto
+import { Link, useNavigate } from "react-router-dom"; // Importa useNavigate aquí
+import { FaSearch } from "react-icons/fa";
+import { FaHome, FaUser } from "react-icons/fa";
+import Categorias from "./categorias.jsx";
+import axios from "axios";
+import Formulario from "../Home/formularioPost.jsx";
 
 const Home = () => {
-    // Estado para almacenar la información del usuario decodificada
-    const [userData, setUserData] = useState(null);
-    const [posts, setPosts] = useState([]);
-    const [showForm, setShowForm] = useState(false);
-    const [searchDisabled, setSearchDisabled] = useState(false);
-    let buttonText = "";
-    let direct = null;
-    const token = localStorage.getItem('token');
-    const tokenExistAndStillValid = token && parseJwt(token).exp * 1000 > Date.now();
+  // Estado para almacenar la información del usuario decodificada
+  const [userData, setUserData] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [searchDisabled, setSearchDisabled] = useState(false);
+  let buttonText = "";
+  let direct = null;
 
-    if (tokenExistAndStillValid) {
-        buttonText = 'Crear Posts';
-        direct = '#';
-    } else {
-        buttonText = 'Iniciar Sesion';
-        direct = '/login';
+  const navigate = useNavigate(); // Define navigate aquí
+
+  const handleLogout = () => {
+    // Eliminar el token JWT del almacenamiento local
+    localStorage.removeItem('token');
+    
+    // Recargar la página
+    window.location.reload();
+  };
+
+  const token = localStorage.getItem("token");
+  const tokenExistAndStillValid =
+    token && parseJwt(token).exp * 1000 > Date.now();
+
+  if (tokenExistAndStillValid) {
+    buttonText = "Crear Posts";
+    direct = "#";
+  } else {
+    buttonText = "Iniciar Sesion";
+    direct = "/login";
+  }
+
+  const handleClick = (event) => {
+    setShowForm(!showForm);
+    setSearchDisabled(!showForm);
+  };
+
+  useEffect(() => {
+    if (token) {
+      const decodedToken = parseJwt(token);
+      setUserData(decodedToken);
     }
+  }, [token]);
 
-    const handleClick = (event) => {
-        setShowForm(!showForm);
-        setSearchDisabled(!showForm);
-    };
+  const handleCategoriaClick = async (categoriaId) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/consultaPostCat?categoriaId=${categoriaId}`
+      );
+      setPosts(response.data);
+    } catch (error) {
+      console.error("Error al cargar los posts:", error);
+    }
+  };
 
-    useEffect(() => {
-        if (token) {
-            const decodedToken = parseJwt(token);
-            setUserData(decodedToken);
-        }
-    }, [token]);
-
-    const handleCategoriaClick = async (categoriaId) => {
-        try {
-            const response = await axios.get(`http://localhost:3000/consultaPostCat?categoriaId=${categoriaId}`);
-            setPosts(response.data);
-        } catch (error) {
-            console.error('Error al cargar los posts:', error);
-        }
-    };
-
-    return (
-        <>
-            <div className='contedorPrincipal'>
-                <div className='barra-navegacion'>
-                    <div className="logo-container">
-                        <img src="src/componentesFront/Login/images/logoApp1.png" alt="Logo" />
-                    </div>
-                    <div className="buscador">
-                        <input className='inputB' disabled={searchDisabled} type="text" placeholder="Search" />
-                        <FaSearch className="iconoBuscar" />
-                    </div>
-                    <Link
-                        className='btnInicioSesion'
-                        id='btnP'
-                        onClick={tokenExistAndStillValid ? handleClick : null}
-                        to={direct}
-                        style={{
-                            opacity: !showForm ? 1 : 0,
-                            pointerEvents: !showForm ? 'auto' : 'none',
-                        }}
-                    >
-                        {buttonText}
-                    </Link>
-                </div>
-                <div className='cont'>
-                    <div className='contIzquierdo'>
-                        <div className='contNav'>
-                            <Link className='btnNav' to="/"><FaHome size={25} className='icon' /> Home</Link>
-                            <Link className='btnNav' to="/perfil"><FaUser size={25} className='icon' /> Perfil</Link>
-                        </div>
-                        <div className='contCategorias'>
-                            <button id="botonPrincipal">Categorias</button>
-                            <Categorias onCategoriaClick={handleCategoriaClick} />
-                        </div>
-                    </div>
-                    <div className='contCentral'>
-                        {showForm && <Formulario onClose={handleClick} />}
-                        {posts.length > 0 ? (
-                            posts.map((post) => (
-                                <div key={post.idPost} className="post">
-                                    <h3>{post.titulo}</h3>
-                                    <p>{post.contenido}</p>
-                                    <p><strong>Dueño:</strong> {post.dueño}</p>
-                                    <p><strong>Fecha de Publicación:</strong> {new Date(post.fechaPublicacion).toLocaleDateString()}</p>
-                                    {post.urlImagen && <img src={post.urlImagen} alt="Imagen del post" />}
-                                    {post.urlDocumento && <a href={post.urlDocumento} target="_blank" rel="noopener noreferrer">Ver Documento</a>}
-                                </div>
-                            ))
-                        ) : (
-                            <p>No hay posts disponibles.</p>
-                        )}
-                    </div>
-                    <div className='contDerecho' style={{ display: showForm ? 'none' : 'block' }}>
-                        <div className='contenidoD'>
-                            <div className='tituloDerecho'> DESARROLLADORES: </div>
-                            <div className='contenedorImagen'>
-                                <img
-                                    src={"src/componentesFront/Login/images/iconoMichael.png"}
-                                    alt="Miniatura"
-                                    style={{ width: '50px', height: '50px' }}
-                                />
-                                <label>Michael Chavez</label>
-                            </div>
-                            <div className='contenedorImagen'>
-                                <img
-                                    src={"src/componentesFront/Login/images/perfilD.jpg"}
-                                    alt="Miniatura"
-                                    style={{ width: '50px', height: '50px' }}
-                                />
-                                <label>David Giler</label>
-                            </div>
-                            <div className='contenedorImagen'>
-                                <img
-                                    src={""}
-                                    alt="Miniatura"
-                                    style={{ width: '50px', height: '50px' }}
-                                />
-                                <label>Kevin Peñafiel</label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="contedorPrincipal">
+        <div className="barra-navegacion">
+          <div className="logo-container">
+            <img
+              src="src/componentesFront/Login/images/logoApp1.png"
+              alt="Logo"
+            />
+          </div>
+          <div className="buscador">
+            <input
+              className="inputB"
+              disabled={searchDisabled}
+              type="text"
+              placeholder="Search"
+            />
+            <FaSearch className="iconoBuscar" />
+          </div>
+          <Link
+            className="btnInicioSesion"
+            id="btnP"
+            onClick={tokenExistAndStillValid ? handleClick : null}
+            to={direct}
+            style={{
+              opacity: !showForm ? 1 : 0,
+              pointerEvents: !showForm ? "auto" : "none",
+            }}
+          >
+            {buttonText}
+          </Link>
+        </div>
+        <div className="cont">
+          <div className="contIzquierdo">
+            <div className="contNav">
+              <Link className="btnNav" to="/">
+                <FaHome size={25} className="icon" /> Home
+              </Link>
+              <Link className="btnNav" to="/perfil">
+                <FaUser size={25} className="icon" /> Perfil
+              </Link>
             </div>
-        </>
-    );
-}
+            <div className="contCategorias">
+              <button id="botonPrincipal">Categorias</button>
+              <Categorias onCategoriaClick={handleCategoriaClick} />
+            </div>
+          </div>
+          <div className="contCentral">
+            {showForm && <Formulario onClose={handleClick} />}
+            {posts.length > 0 ? (
+              posts.map((post) => (
+                <div key={post.idPost} className="post">
+                  <h3>{post.titulo}</h3>
+                  <p>{post.contenido}</p>
+                  <p>
+                    <strong>Dueño:</strong> {post.dueño}
+                  </p>
+                  <p>
+                    <strong>Fecha de Publicación:</strong>{" "}
+                    {new Date(post.fechaPublicacion).toLocaleDateString()}
+                  </p>
+                  {post.urlImagen && (
+                    <img src={post.urlImagen} alt="Imagen del post" />
+                  )}
+                  {post.urlDocumento && (
+                    <a
+                      href={post.urlDocumento}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Ver Documento
+                    </a>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p>No hay posts disponibles.</p>
+            )}
+          </div>
+          <div
+            className="contDerecho"
+            style={{ display: showForm ? "none" : "block" }}
+          >
+            <div className="contenidoD">
+              <div className="tituloDerecho"> DESARROLLADORES: </div>
+              <div className="contenedorImagen">
+                <img
+                  src={"src/componentesFront/Login/images/iconoMichael.png"}
+                  alt="Miniatura"
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <label>Michael Chavez</label>
+              </div>
+              <div className="contenedorImagen">
+                <img
+                  src={"src/componentesFront/Login/images/perfilD.jpg"}
+                  alt="Miniatura"
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <label>David Giler</label>
+              </div>
+              <div className="contenedorImagen">
+                <img
+                  src={""}
+                  alt="Miniatura"
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <label>Kevin Peñafiel</label>
+              </div>
+              <button onClick={handleLogout}>Cerrar Sesión</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
