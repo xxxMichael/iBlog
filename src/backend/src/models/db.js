1eb896a22f219a26f89ac6a152320b44c418aac0
David Giler Rizzo <123764929+Axios21@users.noreply.github.com>
2024-06-07 09:54:07 -0500

    a

--- src/backend/src/models/db.js
+++ src/backend/src/models/db.js
@@ -1,36 +1,13 @@
 const mysql = require('mysql2');
 
-const dbConfig = {
-    host: 'db-iblog.cfoko6wgu4ul.sa-east-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'Ndgr212004',
-    database: 'iBlog'
-};
+const pool = mysql.createPool({
+  host: 'db-iblog.cfoko6wgu4ul.sa-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Ndgr212004',
+  database: 'iBlog',
+  waitForConnections: true,
+  connectionLimit: 10, 
+  queueLimit: 0
+});
 
-let connection;
-
-function handleDisconnect() {
-    connection = mysql.createConnection(dbConfig);
-
-    connection.connect(function(err) {
-        if (err) {
-            console.error('Error conectando a la base de datos:', err);
-            setTimeout(handleDisconnect, 2000); // Intenta reconectar después de 2 segundos
-        } else {
-            console.log('Conexión establecida.');
-        }
-    });
-
-    connection.on('error', function(err) {
-        console.error('Error en la conexión a la base de datos:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect(); // Reconecta si la conexión se pierde
-        } else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+module.exports = pool;
