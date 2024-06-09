676ef0c5f988104b7688c3cd60183b99d7767dd6
xxxMichael <mayckolchavez27@gmail.com>
2024-06-09 15:14:07 -0500

    exclusioncertifcado

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,24 +1,13 @@
 /* eslint-disable no-undef */
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const app = express();
 const port = 3000;
-const routes = require('./src/api/endPoints');
+const routes = require('./src/api/endPoints')
 const cors = require('cors');
-
-// Configuración para leer los certificados SSL
-const options = {
-    key: fs.readFileSync('./src/certificates/key.pem'),
-    cert: fs.readFileSync('./src/certificates/csr.pem')
-};
-
 app.use(express.json()); //interpreta el json
 app.use(express.urlencoded({ extended: true }));
 app.use(cors()); // Permitir solicitudes desde cualquier origen
-app.use('/', routes);
-
-// Crear servidor HTTPS
-https.createServer(options, app).listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+app.use('/', routes)
+app.listen(port, () => {
+    console.log(`listen on port  ${port} `)
+})
