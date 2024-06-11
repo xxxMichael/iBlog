aff78daa6f9cd52bae4bb7067ffc772c8ecdcc2c
David Giler Rizzo <123764929+Axios21@users.noreply.github.com>
2024-06-11 12:30:06 -0500

    for

--- src/componentesFront/Home/formularioPost.jsx
+++ src/componentesFront/Home/formularioPost.jsx
@@ -3,6 +3,7 @@ import './formularioPost.css';
 import { ComponentChecklist } from './Categorias2.jsx';
 import axios from 'axios';
 import { parseJwt } from "../Main/Main";
+import { host } from './Home';
 export function decodificar(token) {
     const base64Url = token.split(".")[1];
     const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -109,6 +110,7 @@ function Formulario({ onClose }) {
     }
     const handleImageChange = (e) => {
         const file = e.target.files[0];
+        console.log(file);
         if (file && (file.type === 'image/jpg' || !file.type === 'image/jpeg')) {
             const reader = new FileReader();
             reader.onloadend = () => {
@@ -118,31 +120,44 @@ function Formulario({ onClose }) {
             reader.readAsDataURL(file);
         }
     };
-    async function controlSubida(event){
-        event.preventDefault(); 
+    async function controlSubida(event) {
+        event.preventDefault();
 
-        if(!archivo){console.log("sube un archivo");}
-        else{
+        if (!archivo) { console.log("sube un archivo"); }
+        else {
             const formData = new FormData();
             formData.append('file', archivo);
 
             // Enviar la imagen al servidor
-            await axios.post("http://localhost:3000/subida", formData, {
-                headers: {'Content-Type': 'multipart/form-data',},
+            await axios.post(`http://${host}:3000/subida`, formData, {
+                headers: { 'Content-Type': 'multipart/form-data', },
             })
-            .then(async function(response){
-                console.log(response);
+                .then(async function (response) {
+                    console.log(response);
 
-                if(response.status===200){
-                    console.log("exito"); 
-                    let urlImagen = response.data.urlImagen; 
-                    setAvatar(urlImagen);
-                }
-                else{ console.log("error");  }
-            });
+                    if (response.status === 200) {
+                        console.log("exito");
+                        let urlImagen = response.data.urlImagen;
+                        setAvatar(urlImagen);
+                    }
+                    else { console.log("error"); }
+                });
         }
 
     }
+    function controlCambioImagen(event) {
+
+        const archivoSubido = event.target.files[0]; // imagen
+
+        if (archivoSubido) {
+            const fileName = archivoSubido.name.toLowerCase();
+
+            // Solamente archivos jpg
+            if (fileName.endsWith('.jpg') && !fileName.endsWith('.jpeg')) { setArchivo(archivoSubido); } // si es jpg, entonces guardamos en archivo.
+            else { console.log("sube un archivo .jpg"); }
+        }
+    }
+
     const handleDivClick = () => {
         document.getElementById('fileInput').click();
     };
@@ -191,6 +206,17 @@ function Formulario({ onClose }) {
                         <div onClick={controlSubida} className="btnEnviar1">Send</div>
                         <div onClick={onClose} className="btnCancelar1">Cancelar</div>
                     </div>
+                </div>
+                <div className='container text-center my-5'>
+                    <h1>AWS S3</h1>
+                    <p>Selecciona una opción</p>
+                    <img className='claseAvatar my-5' src={avatar} alt="avatar" />
+
+
+                    <form>
+                        <div className="mb-3"> <input className="form-control" accept=".jpg" type="file" id="formFile" onChange={controlCambioImagen} /> </div>
+                        <button type="submit" className="btn btn-primary w-100" onClick={controlSubida} >Subir foto</button>
+                    </form>
                 </div>
             </div>
         </div>
