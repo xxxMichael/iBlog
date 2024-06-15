c03dc1df169a10aecdda863b0d5ffb07a9cf5229
xxxMichael <mayckolchavez27@gmail.com>
2024-06-15 01:12:53 -0500

    postSinGet

--- src/backend/src/controllers/consultaPostCat.js
+++ src/backend/src/controllers/consultaPostCat.js
@@ -7,7 +7,7 @@ module.exports.consultaPostCat = (req, res) => {
         return res.status(400).json({ error: 'El ID de la categoría es requerido' });
     }
 
-    if (categoriaId === '*') {
+    if (categoriaId === '*' || (Array.isArray(categoriaId) && categoriaId.includes('*'))) {
         const query = `
         SELECT p.idPost, p.dueño, p.titulo, p.contenido, p.fechaPublicacion, p.urlImagen, p.urlDocumento
         FROM posts p
@@ -15,6 +15,25 @@ module.exports.consultaPostCat = (req, res) => {
         LIMIT 30;
         `;
         connection.query(query, (error, results) => {
+            if (error) {
+                console.error('Error al obtener los posts:', error);
+                return res.status(500).json({ error: 'Error al obtener los posts' });
+            }
+            res.status(200).json(results);
+        });
+    } else if (Array.isArray(categoriaId)) {
+        const placeholders = categoriaId.map(() => '(p.idCategoria1 = ? OR p.idCategoria2 = ? OR p.idCategoria3 = ?)').join(' OR ');
+        const query = `
+        SELECT p.idPost, p.dueño, p.titulo, p.contenido, p.fechaPublicacion, p.urlImagen, p.urlDocumento
+        FROM posts p
+        WHERE ${placeholders};
+        `;
+        const queryParams = [];
+        categoriaId.forEach(id => {
+            queryParams.push(id, id, id);
+        });
+
+        connection.query(query, queryParams, (error, results) => {
             if (error) {
                 console.error('Error al obtener los posts:', error);
                 return res.status(500).json({ error: 'Error al obtener los posts' });
