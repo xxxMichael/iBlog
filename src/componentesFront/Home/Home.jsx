19fa725d68a2de3a9abd06e8f3a7e22104b67d5a
David Giler Rizzo <123764929+Axios21@users.noreply.github.com>
2024-06-15 05:25:22 -0500

    Añadir descarga de Archivos

--- src/componentesFront/Home/Home.jsx
+++ src/componentesFront/Home/Home.jsx
@@ -220,6 +220,13 @@ const Home = () => {
   const clickCategorias = () => {
     setIsCategorias(!isCategorias);
   };
+
+  const getFileNameFromUrl = (url) => {
+    const path = url.split('/').pop();
+    const fileName = path.split('?')[0];
+    return fileName;
+  }
+
   return (
     <>
       <div className="contedorPrincipal">
@@ -245,7 +252,7 @@ const Home = () => {
           {isMobile ? (
             <div className={`overlay ${isCategorias ? 'show' : ''}`}>
               <button onClick={clickCategorias} className={`btn-menu-categorias ${isCategorias ? 'active' : ''}`}>
-              ➤
+                ➤
               </button>
               {isCategorias && (
                 <div className="cont-categorias-des">
@@ -307,14 +314,21 @@ const Home = () => {
                           <svg fill="black" viewBox="0 0 512 512" height="1.5em"><path d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z"></path></svg>
                         </span>
                       </button>
-                      {post.urlArchivo && (
-                        <button className="archivoBtn" onClick={() => handleArchivoClick(post.urlArchivo)}>
-                          <span className="IconContainer">
-                            <svg fill="black" viewBox="0 0 24 24" height="1.5em">
-                              <path d="M5 4v16h14V8.586L13.414 4H5zm6 2v4H7v4h4v4h4v-4h4v-4h-4V6h-4z" />
-                            </svg>
-                          </span>
-                        </button>
+                      {post.urlDocumento && (
+                        <a href={post.urlDocumento} className="Btn-descarga">
+                          <svg
+                            className="svgIcon-descarga"
+                            viewBox="0 0 384 512"
+                            height="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+                            ></path>
+                          </svg>
+                          <span className="icon2-descarga"></span>
+                          <span className="tooltip-descarga">{getFileNameFromUrl(post.urlDocumento)}</span>
+                        </a>
                       )}
                     </div>
                     {selectedPostId === post.idPost && (
