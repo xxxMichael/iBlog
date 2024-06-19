89a74cff59706f9a4163ef2ce4acab30bc3a606a
David Giler Rizzo <123764929+Axios21@users.noreply.github.com>
2024-06-19 04:51:47 -0500

    Arreglo de categorias

--- src/componentesFront/Home/categorias.jsx
+++ src/componentesFront/Home/categorias.jsx
@@ -1,35 +1,38 @@
 // Categorias.jsx
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { host } from "./Home";
-const Categorias = ({ onCategoriaClick }) => {
-  const [categorias, setCategorias] = useState([]);
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { host } from './Home';
 
-  useEffect(() => {
-    const fetchCategorias = async () => {
-      try {
-        const response = await axios.get(
-            `https://${host}/consultarCatego`    
-        );
-        setCategorias(response.data);
-      } catch (error) {
-        console.error("Error al obtener las categorías:", error);
-      }
-    };
-    fetchCategorias();
-  }, []);
+const Categorias = ({ onCategoriaClick,setCategoriasEnHome  }) => {
+    const [categorias, setCategorias] = useState([]);
 
-  return (
-    <ul>
-      {categorias.map((categoria) => (
-        <li key={categoria.id}>
-          <button onClick={() => onCategoriaClick(categoria.id)}>
-            {categoria.nombre}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+    useEffect(() => {
+        const fetchCategorias = async () => {
+            try {
+                const response = await axios.get(`https://${host}/consultarCatego`);
+                setCategorias(response.data);
+                if (setCategoriasEnHome && typeof setCategoriasEnHome === 'function') {
+                    setCategoriasEnHome(response.data); // Pasa las categorías al componente padre (Home)
+                }
+
+            } catch (error) {
+                console.error('Error al obtener las categorías:', error);
+            }
+        };
+        fetchCategorias();
+    }, []);
+
+    return (
+        <ul>
+            {categorias.map((categoria) => (
+                <li key={categoria.id}>
+                    <button className='btns-Categorias' onClick={() => onCategoriaClick(categoria.id)}>
+                        {categoria.nombre}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
 };
 
 export default Categorias;
